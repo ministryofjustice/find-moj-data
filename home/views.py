71b3fa599140ac83d21b7fbe7742bb5b3b925bae
Mat Moore <MatMoore@users.noreply.github.com>
2024-01-12 16:58:31 +0000

    Add sample data for the prototype

--- home/views.py
+++ home/views.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.shortcuts import render
 
 
@@ -11,4 +12,5 @@ def home_view(request):
 def search_view(request):
     context = {}
     context["service_name"] = "Daap Data Catalogue"
+    context.update(settings.SAMPLE_SEARCH_RESULTS)
     return render(request, "search.html", context)
