42deb44c4ff623f4d4cfd189e562957ce583e84e
priya.basker <shanmugapriya.basker@digital.justice.gov.uk>
2024-01-19 14:08:20 +0000

    Add pagination template

--- home/views.py
+++ home/views.py
@@ -5,12 +5,10 @@ from django.shortcuts import render
 # Create your views here.
 def home_view(request):
     context = {}
-    context["service_name"] = "Daap Data Catalogue"
     return render(request, "home.html", context)
 
 
 def search_view(request):
     context = {}
-    context["service_name"] = "Daap Data Catalogue"
     context.update(settings.SAMPLE_SEARCH_RESULTS)
     return render(request, "search.html", context)
