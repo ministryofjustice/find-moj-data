9506b20f63be42560fe20297264d9e1b85a05be6
priya.basker <shanmugapriya.basker@digital.justice.gov.uk>
2024-01-19 16:36:39 +0000

    Added filter functionality

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,5 @@ app_name = "home"
 urlpatterns = [
     path("", views.home_view, name="home"),
     path("search", views.search_view, name="search"),
+    path("search/filter", views.filter_view, name="filter"),
 ]
--- home/views.py
+++ home/views.py
@@ -2,11 +2,23 @@ from django.conf import settings
 from django.shortcuts import render
 
 
+def filter_json_dict(json_dict, target_value):
+    filtered_dict = {key: value for key, value in json_dict.items() if value == target_value}
+    return filtered_dict
+
 # Create your views here.
 def home_view(request):
     context = {}
     return render(request, "home.html", context)
 
+def filter_view(request):
+    data = {}
+    data.update(settings.SAMPLE_SEARCH_RESULTS)
+
+    filtered_results =[item for item in data['results'] if item['domain_name'] == 'HMPPS']
+    context={'results': filtered_results, 'total': len(filtered_results)} 
+   
+    return render(request, "search.html", context)
 
 def search_view(request):
     context = {}
