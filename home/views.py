152afcc5a5766dfa01496c6b25ff4b56437e9e7b
Murdo Moyse <murdo.moyse1@justice.gov.uk>
2024-01-23 09:34:52 +0000

    stash changes

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.shortcuts import render
+import requests
 
 
 # Create your views here.
@@ -12,5 +13,12 @@ def home_view(request):
 def search_view(request):
     context = {}
     context["service_name"] = "Daap Data Catalogue"
+
+    url = "https://datahub.apps-tools.development.data-platform.service.justice.gov.uk/api/graphql"
+    token = "graphQL api token"
+    headers = {"Content-Type": "application/json",  "Authorization": f"Bearer {token}"}
+    body = open("home/get-data-products.graphql").read()
+    get_data_products_graphql_response = requests.post(url, headers=headers, json={"query": body})
+
     context.update(settings.SAMPLE_SEARCH_RESULTS)
     return render(request, "search.html", context)
