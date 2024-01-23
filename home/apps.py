b054e299f6e0a8d246a7cf9bbdb7ce29ef87cb4a
Murdo Moyse <murdo.moyse1@justice.gov.uk>
2024-01-23 13:56:52 +0000

    Created custom class to call GraphQL API

--- home/apps.py
+++ home/apps.py
@@ -1,6 +1,28 @@
 from django.apps import AppConfig
+import requests
+import os
+
+DATAHUB_GRAPHQL_API = os.getenv("DATAHUB_GRAPHQL_API", "https://datahub.apps-tools.development.data-platform.service.justice.gov.uk/api/graphql")
+DATAHUB_GRAPHQL_TOKEN = os.getenv("DATAHUB_GRAPHQL_TOKEN", "graphQL api token")
 
 
 class HomeConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "home"
+
+class DataHubGraphQLAPICaller:
+    def __init__(self) -> None:
+        self.api = DATAHUB_GRAPHQL_API
+        self.token = DATAHUB_GRAPHQL_TOKEN
+
+    def query_datahub_graphql_api(self, graphql_query) -> requests.Response:
+        headers = {"Content-Type": "application/json",  "Authorization": f"Bearer {DATAHUB_GRAPHQL_TOKEN}"}
+        json = {"query": graphql_query}
+
+        return requests.post(DATAHUB_GRAPHQL_API, headers=headers, json=json)
+
+    def get_data_products(self) -> requests.Response:
+        """Get all data products from the datahub GraphQL API"""
+        get_data_products_query = open("core/graphql_queries/get-data-products.graphql").read()
+
+        return self.query_datahub_graphql_api(get_data_products_query)
--- home/views.py
+++ home/views.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.shortcuts import render
-import requests
+from apps import DataHubGraphQLAPICaller
 
 
 # Create your views here.
@@ -14,11 +14,8 @@ def search_view(request):
     context = {}
     context["service_name"] = "Daap Data Catalogue"
 
-    url = "https://datahub.apps-tools.development.data-platform.service.justice.gov.uk/api/graphql"
-    token = "graphQL api token"
-    headers = {"Content-Type": "application/json",  "Authorization": f"Bearer {token}"}
-    body = open("home/get-data-products.graphql").read()
-    get_data_products_graphql_response = requests.post(url, headers=headers, json={"query": body})
+    DataHub_GraphQL_API = DataHubGraphQLAPICaller()
+    get_data_products_graphql_response = DataHub_GraphQL_API.get_data_products()
 
     context.update(settings.SAMPLE_SEARCH_RESULTS)
     return render(request, "search.html", context)
