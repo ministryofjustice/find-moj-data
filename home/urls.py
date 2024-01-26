0fc54213b91c7a91931d60d5dfa227cbed8f8a50
priya.basker <shanmugapriya.basker@digital.justice.gov.uk>
2024-01-26 18:20:38 +0000

    Added filter functionality

--- home/urls.py
+++ home/urls.py
@@ -6,7 +6,5 @@ app_name = "home"
 urlpatterns = [
     path("", views.home_view, name="home"),
     path("search", views.search_view, name="search"),
-    path("search/filter", views.filter_view, name="filter"),
-    path("search/clear", views.result_view, name="result_view"),
     path("details", views.details_view, name="details"),
 ]
--- home/views.py
+++ home/views.py
@@ -18,65 +18,43 @@ def details_view(request):
         )
     return render(request, "details.html", context)
 
-def result_view(request):
-    context = {}
-    context.update(settings.SAMPLE_SEARCH_RESULTS)
-    context.update(domainlist)
-    context.update(dpia_list)
-    return render (request, "search.html", context)
-
-def filter_view(request):
-   
-    # Check if we can reload partial search_html page alone
-    data = {}
-    data.update(settings.SAMPLE_SEARCH_RESULTS)
-
-    search_context = {}
-   
-    if request.method == "POST":
-        # data = json.loads(request.body.decode('utf-8'))
-        # print(data.get('href_value'))
-        domain = request.POST.getlist("type")
-        dpia = request.POST.getlist("dpia")
-
-        filtered_results = [
-            item
-            for item in data["results"]
-            if (not domain or (item["domain_name"]).lower() in domain)
-            and (not dpia or (item["dpia"]).lower() in dpia)
-        ]
-
-        sorted_results=sorted(filtered_results, key=lambda x: x.get("database_name", ""), reverse=False)
-
-        search_context.update(
-            {"results": sorted_results, "total": len(filtered_results)}
-        )
-
-        selected_domain = {"selected_domain": domain}
-        search_context.update(selected_domain)
-        selected_dpia = {"selected_dpia": dpia}
-        search_context.update(selected_dpia)
-
-    return render (request, "search.html", search_context)
-
-
 def search_view(request):
+    
     query = request.GET.get("query", "")
     page = request.GET.get("page", None)
 
     client = get_catalogue_client()
+    
     search_results = client.search(query=query, page=page)
-   
     context = {}
     domain_list=search_results.facets['domains']
     context["domainlist"]=domain_list
+    print(request.GET.get('clear_filter'))
 
-    context['selected_domain'] = {domain_list[0].value: domain_list[0].label}
+    if request.GET.getlist("domain"):
+        domains=request.GET.getlist("domain")
+        selected_domain={}
+        for domain in domain_list:
+            if domain.value in domains:
+                selected_domain[domain.value]=domain.label
+       
+        context['selected_domain'] = selected_domain
+        filter_value = [MultiSelectFilter("domains",domains )]
 
-    search_results = client.search(query=query, page=page, filters=[MultiSelectFilter("domains", [domain_list[0].value])])
+    elif request.GET.get('clear_filter') == "True": 
+        print("clear_filter")
+        filter_value =[]
+        context['selected_domain'] ={}
+    else:  
+        print("search")
+        filter_value =[]
+        context['selected_domain'] ={}
+      
+    
+    search_results = client.search(query=query, page=page, filters=filter_value)
+    print(context['selected_domain'] )
 
     context["query"] = query
-    context["service_name"] = "Daap Data Catalogue"
     context["results"] = search_results.page_results
     context["total_results"] = search_results.total_results
     return render(request, "search.html", context)
