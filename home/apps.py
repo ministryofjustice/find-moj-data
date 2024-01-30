2a02fac929f42f4756046de2bac5e873429b3b12
Murdo Moyse <murdo.moyse1@justice.gov.uk>
2024-01-30 15:06:48 +0000

    stash

--- home/apps.py
+++ home/apps.py
@@ -1,28 +1,46 @@
 from django.apps import AppConfig
 import requests
+import json
 import os
 
 DATAHUB_GRAPHQL_API = os.getenv("DATAHUB_GRAPHQL_API", "https://datahub.apps-tools.development.data-platform.service.justice.gov.uk/api/graphql")
-DATAHUB_GRAPHQL_TOKEN = os.getenv("DATAHUB_GRAPHQL_TOKEN", "graphQL api token")
+DATAHUB_GRAPHQL_TOKEN = os.getenv("DATAHUB_GRAPHQL_TOKEN", "")
 
 
 class HomeConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "home"
 
+class GraphQLError(BaseException):
+    pass
+
 class DataHubGraphQLAPICaller:
     def __init__(self) -> None:
         self.api = DATAHUB_GRAPHQL_API
         self.token = DATAHUB_GRAPHQL_TOKEN
+        self.headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {self.token}"
+        }
 
     def query_datahub_graphql_api(self, graphql_query) -> requests.Response:
-        headers = {"Content-Type": "application/json",  "Authorization": f"Bearer {DATAHUB_GRAPHQL_TOKEN}"}
-        json = {"query": graphql_query}
+        try:
+            json_slug = {"query": graphql_query}
+            response = requests.post(self.api, headers=self.headers, json=json_slug)
+        except requests.exceptions.RequestException as e:
+            raise GraphQLError(e)
 
-        return requests.post(DATAHUB_GRAPHQL_API, headers=headers, json=json)
+        return response
 
-    def get_data_products(self) -> requests.Response:
+    @staticmethod
+    def _get_data_products_query() -> str:
+        return open("core/graphql_queries/get-data-products.graphql").read()
+
+    def get_data_products(self) -> dict:
         """Get all data products from the datahub GraphQL API"""
-        get_data_products_query = open("core/graphql_queries/get-data-products.graphql").read()
+        get_data_products_query = self._get_data_products_query()
+        response = self.query_datahub_graphql_api(get_data_products_query)
+        data_products_json = json.loads(response.text)
+        data_products = data_products_json["data"]["search"]
 
-        return self.query_datahub_graphql_api(get_data_products_query)
+        return data_products
--- home/views.py
+++ home/views.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.shortcuts import render
-from apps import DataHubGraphQLAPICaller
+from home.apps import DataHubGraphQLAPICaller
 
 
 # Create your views here.
@@ -15,7 +15,8 @@ def search_view(request):
     context["service_name"] = "Daap Data Catalogue"
 
     DataHub_GraphQL_API = DataHubGraphQLAPICaller()
-    get_data_products_graphql_response = DataHub_GraphQL_API.get_data_products()
+    data_products_json = DataHub_GraphQL_API.get_data_products()
 
-    context.update(settings.SAMPLE_SEARCH_RESULTS)
+    context.update({"all_data_products": data_products_json["searchResults"]})
+    context.update({"num_data_products": data_products_json["total"]})
     return render(request, "search.html", context)
