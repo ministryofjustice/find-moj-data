882bf512f4540704bda3b8844282c6c2ebc13b9e
Mitch Dawson <Mitchell.Dawson@justice.gov.uk>
2024-02-06 10:34:07 +0000

    update test folder structure

--- tests/home/views/test_views.py
+++ tests/home/views/test_views.py
@@ -87,13 +87,6 @@ class SearchViewTests(SimpleTestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.context["results"]), 20)
 
-    @skip("possibly broken")
-    def test_exposes_domains(self):
-        response = self.client.get(reverse("home:search"), data={})
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.context["selected_domain"], {})
-        self.assertGreater(len(response.context["domains"]), 0)
-
     def test_exposes_empty_query(self):
         response = self.client.get(reverse("home:search"), data={})
         self.assertEqual(response.status_code, 200)
@@ -104,27 +97,3 @@ class SearchViewTests(SimpleTestCase):
             reverse("home:search"), data={"query": "foo"})
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.context["query"], "foo")
-
-    @skip("possibly broken")
-    def test_exposes_filter(self):
-        response = self.client.get(
-            reverse("home:search"), data={"domain": ["urn:li:domain:HMCTS"]}
-        )
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(
-            response.context["selected_domain"], {
-                "urn:li:domain:HMCTS": "HMCTS"}
-        )
-        self.assertEqual(len(response.context["domains"]), 0)
-
-    @skip("possibly broken")
-    def test_exposes_filter_and_query(self):
-        response = self.client.get(
-            reverse("home:search"),
-            data={"domain": ["urn:li:domain:HMCTS"], "query": "courts"},
-        )
-        self.assertEqual(
-            response.context["selected_domain"], {
-                "urn:li:domain:HMCTS": "HMCTS"}
-        )
-        self.assertEqual(response.context["query"], "courts")
