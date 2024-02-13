2b3375a127981f938deab7b4e6661ddd65c23bb6
LavMatt <mattlaverty@gmail.com>
2024-02-13 16:42:09 +0000

    move valid form and add service fixtures to conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,11 +3,18 @@ from unittest.mock import MagicMock, patch
 
 import pytest
 from data_platform_catalogue.client import BaseCatalogueClient
-from data_platform_catalogue.search_types import (FacetOption, ResultType,
-                                                  SearchFacets, SearchResponse,
-                                                  SearchResult)
+from data_platform_catalogue.search_types import (
+    FacetOption,
+    ResultType,
+    SearchFacets,
+    SearchResponse,
+    SearchResult,
+)
 from django.test import Client
 from faker import Faker
+from home.service.search import SearchService
+from home.service.details import DetailsService
+from home.forms.search import SearchForm
 
 fake = Faker()
 
@@ -21,8 +28,7 @@ def generate_page(page_size=20):
         results.append(
             SearchResult(
                 id=fake.unique.name(),
-                result_type=choice(
-                    (ResultType.DATA_PRODUCT, ResultType.TABLE)),
+                result_type=choice((ResultType.DATA_PRODUCT, ResultType.TABLE)),
                 name=fake.name(),
                 description=fake.paragraphs(),
             )
@@ -59,7 +65,8 @@ def mock_catalogue():
     mock_catalogue = MagicMock(spec=BaseCatalogueClient)
     mock_fn.return_value = mock_catalogue
     mock_search_response(
-        mock_catalogue, page_results=generate_page(), total_results=100)
+        mock_catalogue, page_results=generate_page(), total_results=100
+    )
     mock_search_facets_response(mock_catalogue, domains=generate_options())
 
     yield mock_catalogue
@@ -69,10 +76,43 @@ def mock_catalogue():
 
 def mock_search_response(mock_catalogue, total_results=0, page_results=()):
     search_response = SearchResponse(
-        total_results=total_results, page_results=page_results)
+        total_results=total_results, page_results=page_results
+    )
     mock_catalogue.search.return_value = search_response
 
 
 def mock_search_facets_response(mock_catalogue, domains):
-    mock_catalogue.search_facets.return_value = SearchFacets(
-        {"domains": domains})
+    mock_catalogue.search_facets.return_value = SearchFacets({"domains": domains})
+
+
+@pytest.fixture
+def valid_form():
+    valid_form = SearchForm(
+        data={
+            "query": "test",
+            "domains": ["urn:li:domain:HMCTS"],
+            "sort": "ascending",
+            "clear_filter": False,
+            "clear_label": False,
+        }
+    )
+    assert valid_form.is_valid()
+
+    return valid_form
+
+
+@pytest.fixture
+def search_context(valid_form):
+    search_service = SearchService(form=valid_form, page="1")
+    context = search_service._get_context()
+    return context
+
+
+@pytest.fixture
+def detail_context(mock_catalogue):
+    mock_catalogue.search.return_value = SearchResponse(
+        total_results=1, page_results=generate_page(page_size=1)
+    )
+    details_service = DetailsService(urn="urn:li:dataProduct:test")
+    context = details_service._get_context()
+    return context
