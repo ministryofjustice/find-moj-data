df7a435f669466639614892c9f1483e71f5567df
Mat Moore <MatMoore@users.noreply.github.com>
2024-02-14 11:29:43 +0000

    Refactor

--- tests/home/end-to-end/test_search_scenarios.py
+++ tests/home/end-to-end/test_search_scenarios.py
@@ -18,25 +18,19 @@ def selenium(live_server):
     selenium.quit()
 
 
-class HasSelenium(Protocol):
-    @property
-    def selenium(self) -> RemoteWebDriver: ...
+class DetailsPage:
+    def __init__(self, selenium):
+        self.selenium = selenium
 
-
-class LayoutHelpers:
-    def primary_heading(self: HasSelenium):
-        return self.selenium.find_element(By.TAG_NAME, "h1")
-
-    def secondary_heading(self: HasSelenium):
+    def secondary_heading(self):
         return self.selenium.find_element(By.TAG_NAME, "h2")
-
-    @property
-    def title(self: HasSelenium):
-        return self.selenium.title
 
 
 class HomePage:
-    def search_nav_link(self: HasSelenium) -> WebElement:
+    def __init__(self, selenium):
+        self.selenium = selenium
+
+    def search_nav_link(self) -> WebElement:
         return self.selenium.find_element(By.LINK_TEXT, "Search")
 
 
@@ -52,42 +46,48 @@ class SearchResultWrapper:
 
 
 class SearchPage:
-    def result_count(self: HasSelenium) -> WebElement:
+    def __init__(self, selenium):
+        self.selenium = selenium
+
+    def primary_heading(self):
+        return self.selenium.find_element(By.TAG_NAME, "h1")
+
+    def result_count(self) -> WebElement:
         return self.selenium.find_element(By.ID, "result-count")
 
-    def first_search_result(self: HasSelenium) -> SearchResultWrapper:
+    def first_search_result(self) -> SearchResultWrapper:
         return SearchResultWrapper(
             self.selenium.find_element(By.ID, "search-results").find_element(
                 By.CSS_SELECTOR, ".govuk-grid-row"
             )
         )
 
-    def search_bar(self: HasSelenium) -> WebElement:
+    def search_bar(self) -> WebElement:
         return self.selenium.find_element(By.NAME, "query")
 
-    def search_button(self: HasSelenium) -> WebElement:
+    def search_button(self) -> WebElement:
         return self.selenium.find_element(By.CLASS_NAME, "search-button")
 
-    def checked_domain_checkboxes(self: HasSelenium) -> list[WebElement]:
+    def checked_domain_checkboxes(self) -> list[WebElement]:
         return self.selenium.find_elements(
             By.CSS_SELECTOR, "input:checked[name='domains']"
         )
 
-    def checked_sort_option(self: HasSelenium) -> WebElement:
+    def checked_sort_option(self) -> WebElement:
         return self.selenium.find_element(By.CSS_SELECTOR, "input:checked[name='sort']")
 
-    def domain_label(self: HasSelenium, name) -> WebElement:
+    def domain_label(self, name) -> WebElement:
         return self.selenium.find_element(By.XPATH, f"//label[ text() = '{name}' ]")
 
-    def sort_label(self: HasSelenium, name) -> WebElement:
+    def sort_label(self, name) -> WebElement:
         return self.selenium.find_element(By.XPATH, f"//label[ text() = '{name}' ]")
 
-    def selected_filter_tags(self: HasSelenium) -> list[WebElement]:
+    def selected_filter_tags(self) -> list[WebElement]:
         return self.selenium.find_elements(
             By.CSS_SELECTOR, ".moj-filter__tag [data-test-id='selected-domain-label']"
         )
 
-    def selected_filter_tag(self: HasSelenium, value) -> WebElement:
+    def selected_filter_tag(self, value) -> WebElement:
         for result in self.selenium.find_elements(
             By.CSS_SELECTOR, ".moj-filter__tag [data-test-id='selected-domain-label']"
         ):
@@ -96,31 +96,46 @@ class SearchPage:
 
         raise Exception(f"No selected filter with text {value}")
 
-    def clear_filters(self: HasSelenium) -> WebElement:
+    def clear_filters(self) -> WebElement:
         return self.selenium.find_element(By.ID, "clear_filter")
 
-    def apply_filters_button(self: HasSelenium) -> WebElement:
+    def apply_filters_button(self) -> WebElement:
         return self.selenium.find_element(
             By.CSS_SELECTOR, 'button[data-test-id="apply-filters"]'
         )
 
-    def current_page(self: HasSelenium) -> WebElement:
+    def current_page(self) -> WebElement:
         return self.selenium.find_element(
             By.CLASS_NAME, "govuk-pagination__item--current"
         )
 
-    def next_page(self: HasSelenium) -> WebElement:
+    def next_page(self) -> WebElement:
         return self.selenium.find_element(
             By.CLASS_NAME, "govuk-pagination__next"
         ).find_element(By.TAG_NAME, "a")
 
-    def previous_page(self: HasSelenium) -> WebElement:
+    def previous_page(self) -> WebElement:
         return self.selenium.find_element(
             By.CLASS_NAME, "govuk-pagination__prev"
         ).find_element(By.TAG_NAME, "a")
 
 
-class TestSearchWithoutJavascriptAndCss(HomePage, SearchPage, LayoutHelpers):
+@pytest.fixture
+def home_page(selenium) -> HomePage:
+    return HomePage(selenium)
+
+
+@pytest.fixture
+def search_page(selenium) -> SearchPage:
+    return SearchPage(selenium)
+
+
+@pytest.fixture
+def details_page(selenium) -> DetailsPage:
+    return DetailsPage(selenium)
+
+
+class TestSearchWithoutJavascriptAndCss:
     """
     Test interacting with the search form through the browser,
     as a user would
@@ -132,31 +147,34 @@ class TestSearchWithoutJavascriptAndCss(HomePage, SearchPage, LayoutHelpers):
         assert output.returncode == 0, output.stdout
 
     @pytest.fixture(autouse=True)
-    def setup(self, live_server, selenium):
+    def setup(self, live_server, selenium, home_page, search_page, details_page):
         self.selenium = selenium
         self.live_server_url = live_server.url
+        self.home_page = home_page
+        self.search_page = search_page
+        self.details_page = details_page
 
     def start_on_the_home_page(self):
         self.selenium.get(f"{self.live_server_url}")
-        assert "Data catalogue" in self.title
+        assert "Data catalogue" in self.selenium.title
 
     def start_on_the_search_page(self):
         self.selenium.get(f"{self.live_server_url}/search?new=True")
-        assert "Search" in self.title
+        assert "Search" in self.selenium.title
 
     def click_on_the_search_button(self):
-        self.search_nav_link().click()
+        self.home_page.search_nav_link().click()
 
     def verify_i_am_on_the_search_page(self):
         assert "Search" in self.selenium.title
-        assert "Find MOJ Data" in self.primary_heading().text
+        assert "Find MOJ Data" in self.search_page.primary_heading().text
 
     def verify_i_have_results(self):
-        result_count = self.result_count().text
+        result_count = self.search_page.result_count().text
         assert re.match(r"[1-9]\d* Results", result_count)
 
     def click_on_the_first_result(self):
-        first_result = self.first_search_result()
+        first_result = self.search_page.first_search_result()
         assert first_result.text
 
         first_link = first_result.link()
@@ -165,44 +183,44 @@ class TestSearchWithoutJavascriptAndCss(HomePage, SearchPage, LayoutHelpers):
         return item_name
 
     def verify_i_am_on_the_details_page(self, item_name):
-        assert item_name in self.title
+        assert item_name in self.selenium.title
 
-        secondary_heading_text = self.secondary_heading().text
+        secondary_heading_text = self.details_page.secondary_heading().text
 
         assert secondary_heading_text == item_name
 
     def enter_a_query_and_submit(self, query):
-        search_bar = self.search_bar()
+        search_bar = self.search_page.search_bar()
         search_bar.send_keys(query)
         self.click_search_button()
 
     def select_domain(self, domains):
         for domain in domains:
-            self.domain_label(domain).click()
+            self.search_page.domain_label(domain).click()
 
     def click_sort_option(self, sortby):
-        self.sort_label(sortby).click()
+        self.search_page.sort_label(sortby).click()
 
     def click_search_button(self):
-        self.search_button().click()
+        self.search_page.search_button().click()
 
     def click_apply_filters(self):
-        self.apply_filters_button().click()
+        self.search_page.apply_filters_button().click()
 
     def click_clear_selected_filter(self, name):
-        self.selected_filter_tag(name).click()
+        self.search_page.selected_filter_tag(name).click()
 
     def click_clear_filters(self):
-        self.clear_filters().click()
+        self.search_page.clear_filters().click()
 
     def verify_the_search_bar_has_value(self, query):
-        search_bar = self.search_bar()
+        search_bar = self.search_page.search_bar()
 
         assert search_bar.get_attribute("value") == query
 
     def verify_domain_selected(self, domains):
         expected = set(domains)
-        checkboxes = self.checked_domain_checkboxes()
+        checkboxes = self.search_page.checked_domain_checkboxes()
         actual = set()
         for checkbox in checkboxes:
             value = checkbox.get_attribute("value") or ""
@@ -211,24 +229,24 @@ class TestSearchWithoutJavascriptAndCss(HomePage, SearchPage, LayoutHelpers):
         assert actual == expected
 
     def verify_selected_filters_shown(self, domains):
-        actual = {i.text for i in self.selected_filter_tags()}
+        actual = {i.text for i in self.search_page.selected_filter_tags()}
         expected = set(domains)
 
         assert actual == expected
 
     def verify_page(self, expected):
-        current_page = self.current_page()
+        current_page = self.search_page.current_page()
 
         assert current_page.text == expected
 
     def click_next_page(self):
-        self.next_page().click()
+        self.search_page.next_page().click()
 
     def click_previous_page(self):
-        self.previous_page().click()
+        self.search_page.previous_page().click()
 
     def verify_sort_selected(self, expected):
-        value = self.checked_sort_option().get_attribute("value") or ""
+        value = self.search_page.checked_sort_option().get_attribute("value") or ""
         assert value == expected.lower()
 
     def test_browse_to_first_item(self):
@@ -319,11 +337,9 @@ class TestSearchWithoutJavascriptAndCss(HomePage, SearchPage, LayoutHelpers):
         self.start_on_the_search_page()
         self.click_next_page()
         self.verify_page("2")
-        current_url = self.selenium.current_url
 
         self.select_domain(["HMPPS"])
         self.click_apply_filters()
-        self.wait_for_url_change(current_url)
         self.verify_page("1")
 
     def test_clear_single_filter(self):
