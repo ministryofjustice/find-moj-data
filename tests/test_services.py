65fc8c5a24e75a5881cfa4f42270e83eed529223
Mat Moore <MatMoore@users.noreply.github.com>
2024-02-19 14:16:15 +0000

    Include subdomains in the filter when we filter by a parent domain
    I've just hardcoded the domains for now, since I just want to get the
    top-level domain filter working as expected again after assigning assets
    to subdomains.

--- tests/test_services.py
+++ tests/test_services.py
@@ -1,5 +1,9 @@
 from types import GeneratorType
+
+import pytest
 from data_platform_catalogue.search_types import ResultType
+
+from home.service.search import domains_with_their_subdomains
 
 
 class TestSearchService:
@@ -7,8 +11,7 @@ class TestSearchService:
         assert search_context["form"] == valid_form
 
     def test_get_context_search_result(self, mock_catalogue, search_context):
-        assert search_context["results"] == mock_catalogue.search(
-        ).page_results
+        assert search_context["results"] == mock_catalogue.search().page_results
         assert search_context["total_results"] == 100
 
     def test_get_context_paginator(self, search_context):
@@ -27,8 +30,7 @@ class TestSearchService:
 
 class TestDetailsService:
     def test_get_context(self, detail_context, mock_catalogue):
-        assert detail_context["result"] == mock_catalogue.search(
-        ).page_results[0]
+        assert detail_context["result"] == mock_catalogue.search().page_results[0]
         result_type = (
             "Data product"
             if mock_catalogue.search().page_results[0].result_type
@@ -40,3 +42,21 @@ class TestDetailsService:
             detail_context["page_title"]
             == f"{mock_catalogue.search().page_results[0].name} - Data catalogue"
         )
+
+
+@pytest.mark.parametrize(
+    "domain, expected_subdomains",
+    [
+        ("does-not-exist", ["does-not-exist"]),
+        (
+            "urn:li:domain:HMPPS",
+            [
+                "urn:li:domain:2feb789b-44d3-4412-b998-1f26819fabf9",
+                "urn:li:domain:abe153c1-416b-4abb-be7f-6accf2abb10a",
+                "urn:li:domain:HMPPS",
+            ],
+        ),
+    ],
+)
+def test_domain_expansion(domain, expected_subdomains):
+    assert domains_with_their_subdomains([domain]) == expected_subdomains
