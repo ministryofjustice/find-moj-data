78fa7101244b173f6b0c295c9f6430bd44905e17
Mitch Dawson <Mitchell.Dawson@justice.gov.uk>
2024-02-24 21:16:08 +0000

    add get_keys and format_label template filter functions

--- home/templatetags/clear_filter.py
+++ home/templatetags/clear_filter.py
@@ -4,12 +4,22 @@ register = template.Library()
 
 
 @register.filter
-def get_item(dictionary, key):
-    print(key)
-    print(dictionary)
+def get_item(dictionary: dict[str, dict], key: str) -> dict[str, list | str]:
     return dictionary.get(key)
 
 
 @register.filter
-def get_items(dictionary, key):
+def get_items(dictionary: dict[str, dict], key: str) -> list:
     return dictionary.get(key).items()
+
+
+@register.filter
+def get_keys(dictionary: dict[str, dict]) -> list[str] | list:
+    if dictionary:
+        return dictionary.keys()
+    return []
+
+
+@register.filter
+def format_label(label: str) -> str:
+    return label.replace("_", " ").title() if "_" in label else label
