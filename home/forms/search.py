5422e053b09573f1b55edbb38fdf95dcef921c36
Mat <MatMoore@users.noreply.github.com>
2024-02-27 16:39:36 +0000

    Update home/forms/search.py
    Co-authored-by: Tom Webber <80110358+tom-webber@users.noreply.github.com>

--- home/forms/search.py
+++ home/forms/search.py
@@ -48,14 +48,14 @@ class SelectWithOptionAttribute(forms.Select):
         self.domain_model = DomainModel()
 
     def create_option(
-        self, name, value, label, selected, index, subindex=None, attrs=None
+        self, name, urn, label, selected, index, subindex=None, attrs=None
     ):
         option = super().create_option(
-            name, value, label, selected, index, subindex, attrs
+            name, urn, label, selected, index, subindex, attrs
         )
 
-        if value:
-            option["attrs"]["data-parent"] = self.domain_model.get_parent_urn(value)
+        if urn:
+            option["attrs"]["data-parent"] = self.domain_model.get_parent_urn(urn)
 
         return option
 
