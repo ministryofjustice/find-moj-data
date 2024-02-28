321b466fd6b925d208d9f8600d10bc21eb974a32
Mat Moore <MatMoore@users.noreply.github.com>
2024-02-28 14:16:23 +0000

    Hide term groups in response to the filter

--- static/assets/js/enhanced-glossary.js
+++ static/assets/js/enhanced-glossary.js
@@ -10,13 +10,30 @@ const updateResults = () => {
   const input = document.getElementById("filter-input");
   const filter = input.value.toUpperCase();
   const termElements = document.querySelectorAll(".term");
+  const termGroups = document.querySelectorAll(".term-group");
 
   // Loop through all terms, and hide those who don't match the search query
   termElements.forEach((el) => {
-    if (filter == "" || el.dataset.term.startsWith(filter)) {
+    if (
+      filter == "" ||
+      el.dataset.term.startsWith(filter) ||
+      el.dataset.term.includes(" " + filter)
+    ) {
       el.classList.remove("govuk-!-display-none");
     } else {
       el.classList.add("govuk-!-display-none");
+    }
+  });
+
+  // Loop through all term groups, and hide those without visible terms
+  termGroups.forEach((el) => {
+    const terms = el.querySelectorAll(".term:not(.govuk-\\!-display-none)");
+    const isEmpty = terms.length === 0;
+
+    if (isEmpty) {
+      el.classList.add("govuk-!-display-none");
+    } else {
+      el.classList.remove("govuk-!-display-none");
     }
   });
 };
