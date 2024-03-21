1ad354c3d94e076d5693356f13d461e3d4455742
LavMatt <matthew.laverty@justice.gov.uk>
2024-03-21 10:25:38 +0000

    add database details service and make datasets work for multiple parent types

--- home/service/details.py
+++ home/service/details.py
@@ -56,6 +56,58 @@ class DataProductDetailsService(GenericService):
         return context
 
 
+class DatabaseDetailsService(GenericService):
+    def __init__(self, urn: str):
+        self.urn = urn
+        self.client = self._get_catalogue_client()
+
+        filter_value = [MultiSelectFilter("urn", [urn])]
+        search_results = self.client.search(query="", page=None, filters=filter_value)
+
+        if not search_results.page_results:
+            raise ObjectDoesNotExist(urn)
+
+        self.result = search_results.page_results[0]
+        self.entities_in_database = self._get_database_entities()
+        self.context = self._get_context()
+
+    def _get_database_entities(self):
+        # we might want to implement pagination for data product children
+        # details at some point
+        database_search = self.client.list_database_tables(
+            urn=self.urn, count=500
+        ).page_results
+
+        entities_in_database = []
+        for result in database_search:
+            entities_in_database.append(
+                {
+                    "name": result.name,
+                    "urn": result.id,
+                    "description": result.description,
+                    "type": "TABLE",
+                }
+            )
+
+        entities_in_database = sorted(entities_in_database, key=lambda d: d["name"])
+
+        return entities_in_database
+
+    def _get_context(self):
+        context = {
+            "result": self.result,
+            "result_type": (
+                "Database"
+                if self.result.result_type == ResultType.DATABASE
+                else "Table"
+            ),
+            "tables": self.entities_in_database,
+            "page_title": f"{self.result.name} - Data catalogue",
+        }
+
+        return context
+
+
 class DatasetDetailsService(GenericService):
     def __init__(self, urn: str):
         super().__init__()
@@ -64,30 +116,32 @@ class DatasetDetailsService(GenericService):
 
         self.table_metadata = self.client.get_table_details(urn)
 
-        # Use a search query to obtain the remaining metadata about the table.
-        # This could be merged into the above query.
-        filter_value = [MultiSelectFilter("urn", [urn])]
-        search_results = self.client.search(query="", page=None, filters=filter_value)
-
-        if not search_results.page_results:
+        if not self.table_metadata:
             raise ObjectDoesNotExist(urn)
 
-        self.result = search_results.page_results[0]
-        data_products = self.result.metadata.get("data_products", ())
-        if data_products:
-            # Pick the first data product to use as the parent in the breadcrumb.
-            # If the dataset belongs to multiple data products, this may diverge
+        self.table_metadata
+        parents = self.table_metadata.relationships
+        if parents:
+            # Pick the first entity to use as the parent in the breadcrumb.
+            # If the dataset belongs to multiple parents, this may diverge
             # from the path the user took to get to this page. However as of datahub
-            # v0.12, assigning to multiple data products is not possible.
-            self.parent_data_product = data_products[0]
+            # v0.12, assigning to multiple data products is not possible and we don't
+            # have datasets with multiple parent containers.
+            self.parent_entity = parents["entities"][0]
+            self.dataset_parent_type = (
+                ResultType.DATABASE.name.lower()
+                if "container" in self.parent_entity["id"].split(":")
+                else ResultType.DATA_PRODUCT.name.lower()
+            )
         else:
-            self.parent_data_product = None
+            self.parent_entity = None
+            self.dataset_parent_type = None
 
         self.context = self._get_context()
 
     def _get_context(self):
         return {
             "table": self.table_metadata,
-            "parent_data_product": self.parent_data_product,
-            "result": self.result,
+            "parent_entity": self.parent_entity,
+            "dataset_parent_type": self.dataset_parent_type,
         }
