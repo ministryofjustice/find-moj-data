17306e10f715f7a0e41136c0968c7b84469f8405
Murdo Moyse <murdo.moyse@digital.justice.gov.uk>
2024-05-10 15:35:26 +0100

    Centralised static import script

---
+++ scripts/import-static.sh
@@ -0,0 +1,12 @@
+rm -rf static/assets/images \
+    && cp -a node_modules/govuk-frontend/dist/govuk/assets/images static/assets/images \
+    && cp -a node_modules/@ministryofjustice/frontend/moj/assets/images/. static/assets/images
+
+rm -rf static/assets/fonts \
+    && cp -r node_modules/govuk-frontend/dist/govuk/assets/fonts static/assets/fonts
+
+cp node_modules/govuk-frontend/dist/govuk/govuk-frontend.min.js static/assets/js/govuk-frontend.min.js \
+    && cp node_modules/govuk-frontend/dist/govuk/govuk-frontend.min.js.map static/assets/js/govuk-frontend.min.js.map
+    # && cp node_modules/@ministryofjustice/frontend/moj/ static/assets/js/@ministryofjustice/frontend/moj/
+
+sass --load-path=. scss:static/assets/css
