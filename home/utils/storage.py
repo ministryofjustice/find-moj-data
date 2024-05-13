1b0058d5297d0573b34d6c4171cd3f06413b3cb9
Murdo Moyse <murdo.moyse@digital.justice.gov.uk>
2024-05-13 16:02:20 +0100

    Manifest strict

--- home/utils/storage.py
+++ home/utils/storage.py
@@ -1,4 +1,7 @@
+from typing import Any
 from django.contrib.staticfiles.storage import ManifestStaticFilesStorage
 
 class NonstrictManifestStaticFilesStorage(ManifestStaticFilesStorage):
-    manifest_strict = False
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+        self.manifest_strict = False
