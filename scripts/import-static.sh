0b7a24052c878bda28cc401307253ff3a5d1e401
Murdo Moyse <murdo.moyse@digital.justice.gov.uk>
2024-05-13 09:49:30 +0100

    Standardised cp operations

--- scripts/import-static.sh
+++ scripts/import-static.sh
@@ -3,10 +3,10 @@ rm -rf static/assets/images \
     && cp -a node_modules/@ministryofjustice/frontend/moj/assets/images/. static/assets/images
 
 rm -rf static/assets/fonts \
-    && cp -r node_modules/govuk-frontend/dist/govuk/assets/fonts static/assets/fonts
+    && cp -a node_modules/govuk-frontend/dist/govuk/assets/fonts static/assets/fonts
 
-cp node_modules/govuk-frontend/dist/govuk/govuk-frontend.min.js static/assets/js/govuk-frontend.min.js \
-    && cp node_modules/govuk-frontend/dist/govuk/govuk-frontend.min.js.map static/assets/js/govuk-frontend.min.js.map \
-    && cp node_modules/@ministryofjustice/frontend/moj/all.jquery.min.js static/assets/js/moj-frontend.min.js
+cp -a node_modules/govuk-frontend/dist/govuk/govuk-frontend.min.js static/assets/js/govuk-frontend.min.js \
+    && cp -a node_modules/govuk-frontend/dist/govuk/govuk-frontend.min.js.map static/assets/js/govuk-frontend.min.js.map \
+    && cp -a node_modules/@ministryofjustice/frontend/moj/all.jquery.min.js static/assets/js/moj-frontend.min.js
 
 sass --load-path=. scss:static/assets/css
