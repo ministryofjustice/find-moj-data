dbf87fac3d74ec1faf78ea3db3933e903f093b8b
Tom Webber <thomas.webber@digital.justice.gov.uk>
2024-06-18 09:44:02 +0100

    change metadata specification field names to snake case, code-formatted - output snake case field name, type string from field type formatter - code format field names

--- home/templatetags/format_metadata_field_type.py
+++ home/templatetags/format_metadata_field_type.py
@@ -4,15 +4,15 @@ register = template.Library()
 
 
 @register.filter
-def format_metadata_field_type(value: dict) -> str:
+def format_metadata_field_type(value: dict) -> dict[str, str]:
     if value.get("title"):
-        title = value["title"]
+        title = value["title"].lower().replace(" ", "_")
         if value.get("type"):
             type_string = value["type"]
         if value.get("anyOf"):
             type_string = " or ".join(item["type"] for item in value["anyOf"])
-        output = f"{title} ({type_string})"
+        output = {"field_name": title, "field_type": type_string}
     if value.get("allOf"):
         linked_entity = value["allOf"][0]["$ref"].split("/")[2]
-        output = f"{linked_entity} (object)"
+        output = {"field_name": linked_entity, "field_type": "object"}
     return output
