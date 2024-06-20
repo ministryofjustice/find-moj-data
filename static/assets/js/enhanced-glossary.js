fd7e305d3bcfe31146664a3cc17370d072354533
Mat Moore <MatMoore@users.noreply.github.com>
2024-06-20 16:52:27 +0100

    Make glossary search a search input as well

--- static/assets/js/enhanced-glossary.js
+++ static/assets/js/enhanced-glossary.js
@@ -3,7 +3,7 @@ export const init = () => {
   jsRequired.style.display = "block";
 
   const input = document.getElementById("filter-input");
-  input.addEventListener("keyup", updateResults);
+  input.addEventListener("input", updateResults);
 };
 
 const updateResults = () => {
@@ -30,7 +30,7 @@ const updateResults = () => {
     const terms = Array.from(el.querySelectorAll(".term"));
     const isEmpty =
       terms.length === 0 ||
-      terms.every((term) => term.classList.contains("govuk-!-display-none"));
+    terms.every((term) => term.classList.contains("govuk-!-display-none"));
 
     if (isEmpty) {
       el.classList.add("govuk-!-display-none");
