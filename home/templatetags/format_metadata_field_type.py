1ca4f6471d79cdec8bb30bc9a5dd286b0a98f4f1
Tom Webber <thomas.webber@digital.justice.gov.uk>
2024-06-20 15:36:03 +0100

    simplify fetching entity field name

--- home/templatetags/format_metadata_field_type.py
+++ home/templatetags/format_metadata_field_type.py
@@ -4,15 +4,13 @@ register = template.Library()
 
 
 @register.filter
-def format_metadata_field_type(value: dict) -> dict[str, str]:
+def format_metadata_field_type(value: dict) -> str:
     if value.get("title"):
-        title = value["title"].lower().replace(" ", "_")
         if value.get("type"):
             type_string = value["type"]
         if value.get("anyOf"):
             type_string = " or ".join(item["type"] for item in value["anyOf"])
-        output = {"field_name": title, "field_type": type_string}
+        output = type_string
     if value.get("allOf"):
-        linked_entity = value["allOf"][0]["$ref"].split("/")[2]
-        output = {"field_name": linked_entity, "field_type": "object"}
+        output = "object"
     return output
