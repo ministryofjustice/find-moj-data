6298734ec29ddefc7e3f4a51a7f6ab15c09ed7d8
Mitch Dawson <Mitchell.Dawson@justice.gov.uk>
2024-06-20 13:53:29 +0100

    update tests

--- tests/users/models/test_user.py
+++ tests/users/models/test_user.py
@@ -1,7 +1,6 @@
+import pytest
 from django.contrib.auth import get_user_model
 from django.db.utils import IntegrityError
-
-import pytest
 
 
 @pytest.mark.django_db
@@ -11,14 +10,12 @@ class TestUserModel:
         user_model = get_user_model()
 
         params = {
-            "username": "user1",
             "email": "user1@example.com",
             "first_name": "Joe",
             "last_name": "Bloggs",
         }
         user = user_model.objects.create(**params)
 
-        assert user.username == params["username"]
         assert user.email == params["email"]
         assert user.first_name == params["first_name"]
         assert user.last_name == params["last_name"]
@@ -27,14 +24,13 @@ class TestUserModel:
         user_model = get_user_model()
 
         params = {
-            "username": "user1",
             "email": "user1@example.com",
             "first_name": "Joe",
             "last_name": "Bloggs",
         }
         user_model.objects.create(**params)
 
-        user = user_model.objects.get(username=params["username"])
+        user = user_model.objects.get(email=params["email"])
 
         assert user.email == params["email"]
         assert user.first_name == params["first_name"]
@@ -44,24 +40,22 @@ class TestUserModel:
         user_model = get_user_model()
 
         params = {
-            "username": "user1",
             "email": "user1@example.com",
             "first_name": "Joe",
             "last_name": "Bloggs",
         }
         user = user_model.objects.create(**params)
-        user.username = "user2"
-        user.email = "user2@example.com"
+        user.first_name = "David"
+        user.last_name = "Smith"
         user.save()
 
-        assert user.username == "user2"
-        assert user.email == "user2@example.com"
+        assert user.first_name == "David"
+        assert user.last_name == "Smith"
 
     def test_delete_user(self):
         user_model = get_user_model()
 
         params = {
-            "username": "user1",
             "email": "user1@example.com",
             "first_name": "Joe",
             "last_name": "Bloggs",
@@ -70,37 +64,19 @@ class TestUserModel:
         user.delete()
 
         with pytest.raises(user_model.DoesNotExist) as exc:
-            user_model.objects.get(username=params["username"])
+            user_model.objects.get(email=params["email"])
 
         assert "CustomUser matching query does not exist" in str(exc.value)
-
-    def test_duplicate_username(self):
-        user_model = get_user_model()
-
-        params = {
-            "username": "user1",
-            "email": "user1@example.com",
-            "first_name": "Joe",
-            "last_name": "Bloggs",
-        }
-        user_model.objects.create(**params)
-
-        with pytest.raises(IntegrityError) as exc:
-            user_model.objects.create(**params)
-
-        assert "UNIQUE constraint failed: users_customuser.username" in str(exc.value)
 
     def test_duplicate_email(self):
         user_model = get_user_model()
 
         params = {
-            "username": "user1",
             "email": "user1@example.com",
             "first_name": "Joe",
             "last_name": "Bloggs",
         }
         user_model.objects.create(**params)
-        params["username"] = "user2"
 
         with pytest.raises(IntegrityError) as exc:
             user_model.objects.create(**params)
