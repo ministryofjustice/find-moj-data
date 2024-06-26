b16af60c2f33ee8243659ac2688255cafee14b57
LavMatt <matthew.laverty@justice.gov.uk>
2024-06-26 10:14:25 +0100

    add SearchTagFetcher to app

---
+++ home/service/search_tag_fetcher.py
@@ -0,0 +1,24 @@
+from django.core.cache import cache
+
+from .base import GenericService
+
+
+class SearchTagFetcher(GenericService):
+    def __init__(self):
+        self.client = self._get_catalogue_client()
+        self.cache_key = "search_tags"
+        self.cache_timeout_seconds = 5
+
+    def fetch(self) -> list:
+        """
+        Fetch a static list of options that is independent of the search query
+        and any applied filters. Values are cached for 5 seconds to avoid
+        unnecessary queries.
+        """
+        result = cache.get(self.cache_key)
+        if not result:
+            result = self.client.get_tags()
+
+            cache.set(self.cache_key, result, timeout=self.cache_timeout_seconds)
+
+        return result
