b1c47177dc58af964542ae8fdf95a6ddee14c266
LavMatt <matthew.laverty@justice.gov.uk>
2024-07-22 17:38:26 +0100

    add test and realign function

--- lib/datahub-client/data_platform_catalogue/client/graphql_helpers.py
+++ lib/datahub-client/data_platform_catalogue/client/graphql_helpers.py
@@ -309,6 +309,6 @@ def _make_user_email_from_urn(urn) -> str:
     but for now this will fix the issue of owners being flagged in datahub but not
     showing in find-moj-data
     """
-  username = urn.replace("urn:li:corpuser:", "")
-  email = f"{username}@justice.gov.uk"
+    username = urn.replace("urn:li:corpuser:", "")
+    email = f"{username}@justice.gov.uk"
     return email
