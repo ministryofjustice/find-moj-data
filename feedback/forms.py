1db6452adf0b1935b3898142180ae7852615110b
Mat Moore <MatMoore@users.noreply.github.com>
2024-08-13 14:52:48 +0100

    fix: make sure RadioSelect doesn't have a blank choice

--- feedback/forms.py
+++ feedback/forms.py
@@ -4,6 +4,17 @@ from django.forms.widgets import RadioSelect
 from .models import Feedback
 
 
+def formfield(field, **kwargs):
+    """
+    Wrapper around the db model's formfield method that maps db fields to form fields.
+
+    This is a workaround to prevent a blank choice being included in the ChoiceField.
+    By default, if the model field has no default value (or blank is set to True),
+    then it will include this empty value, even if we have customised the widget is customised.
+    """
+    return field.formfield(initial=None, **kwargs)
+
+
 class FeedbackForm(ModelForm):
     class Meta:
         model = Feedback
@@ -11,3 +22,4 @@ class FeedbackForm(ModelForm):
         widgets = {
             "satisfaction_rating": RadioSelect(attrs={"class": "govuk-radios__input"})
         }
+        formfield_callback = formfield
--- feedback/models.py
+++ feedback/models.py
@@ -16,9 +16,8 @@ class Feedback(models.Model):
         verbose_name=_("Satisfaction survey"),
         null=False,
         blank=False,
-        default=3,
     )
 
     how_can_we_improve = models.TextField(
-        verbose_name=_("How can we improve this service?"), null=False, blank=False
+        verbose_name=_("How can we improve this service?"), null=False, blank=True
     )
