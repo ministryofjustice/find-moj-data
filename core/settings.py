314dd0093c3a867f536ff63b38320f9663fb72c8
Mat Moore <MatMoore@users.noreply.github.com>
2024-09-19 17:05:44 +0100

    Enable flamegraph in django debug toolbar

--- core/settings.py
+++ core/settings.py
@@ -242,6 +242,29 @@ if DEBUG and not TESTING:
     INSTALLED_APPS.insert(-1, "debug_toolbar")
     MIDDLEWARE.insert(1, "debug_toolbar.middleware.DebugToolbarMiddleware")
 
+    DEBUG_TOOLBAR_PANELS = [
+        'debug_toolbar.panels.history.HistoryPanel',
+        'debug_toolbar.panels.versions.VersionsPanel',
+        'debug_toolbar.panels.timer.TimerPanel',
+        'debug_toolbar.panels.settings.SettingsPanel',
+        'debug_toolbar.panels.headers.HeadersPanel',
+        'debug_toolbar.panels.request.RequestPanel',
+        'debug_toolbar.panels.sql.SQLPanel',
+        'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+        'debug_toolbar.panels.templates.TemplatesPanel',
+        'debug_toolbar.panels.alerts.AlertsPanel',
+        'debug_toolbar.panels.cache.CachePanel',
+        'debug_toolbar.panels.signals.SignalsPanel',
+        'debug_toolbar.panels.redirects.RedirectsPanel',
+        'debug_toolbar.panels.profiling.ProfilingPanel',
+        'pyflame.djdt.panel.FlamegraphPanel',
+    ]
+
+    INSTALLED_APPS.append('pyflame')
+
+    PYFLAME_CONFIG = {
+        'FLAMEGRAPH_SCRIPT_PATH': 'bin/flamegraph.pl'
+    }
 # Enable / Disable Azure Auth
 if not os.environ.get("AZURE_AUTH_ENABLED", "true") == "false":
     # Adds the Azure Authentication middleware to the Django Authentication middleware
