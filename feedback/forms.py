053dc5ea2145fc775b70609aa8ca3a85160d0a53
Mitch Dawson <Mitchell.Dawson@justice.gov.uk>
2024-10-17 14:05:41 +0100

    initial commit

--- feedback/forms.py
+++ feedback/forms.py
@@ -1,7 +1,7 @@
-from django.forms import ModelForm
-from django.forms.widgets import RadioSelect
+from django import forms
+from django.forms.widgets import RadioSelect, Textarea, TextInput
 
-from .models import Feedback
+from .models import Feedback, ReportIssue
 
 
 def formfield(field, **kwargs):
@@ -15,11 +15,41 @@ def formfield(field, **kwargs):
     return field.formfield(initial=None, **kwargs)
 
 
-class FeedbackForm(ModelForm):
+class FeedbackForm(forms.ModelForm):
     class Meta:
         model = Feedback
         fields = ["satisfaction_rating", "how_can_we_improve"]
         widgets = {
             "satisfaction_rating": RadioSelect(attrs={"class": "govuk-radios__input"})
+        }
+        formfield_callback = formfield
+
+
+class ReportIssueForm(forms.ModelForm):
+    class Meta:
+        model = ReportIssue
+        fields = [
+            "reason",
+            "additional_info",
+            "user_email",
+        ]
+        widgets = {
+            "reason": RadioSelect(attrs={"class": "govuk-radios__input"}),
+            "additional_info": Textarea(
+                attrs={
+                    "class": "govuk-textarea",
+                    "rows": "5",
+                    "aria-describedby": "more-detail-hint",
+                }
+            ),
+            "user_email": TextInput(
+                attrs={
+                    "class": "govuk-input",
+                    "type": "email",
+                    "spellcheck": "false",
+                    "aria-describedby": "email-hint",
+                    "autocomplete": "email",
+                }
+            ),
         }
         formfield_callback = formfield
