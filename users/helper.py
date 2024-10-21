6284d8d58be3bb151e38661d0188a00bfd4af81e
LavMatt <matthew.laverty@justice.gov.uk>
2024-10-21 08:19:51 +0100

    fix: ensure user emails are lowercase when saved to postgres
    azure was returning emails with cases that had changed and it broke user access

--- users/helper.py
+++ users/helper.py
@@ -2,4 +2,5 @@ def user_mapping_fn(**attributes):
     return {
         "first_name": attributes["givenName"],
         "last_name": attributes["surname"],
+        "email": attributes["mail"].lower(),
     }
