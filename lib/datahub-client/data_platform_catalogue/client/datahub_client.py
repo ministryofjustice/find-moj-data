8c4e12eaf521b4b9fc375cf07a0e47add057e55b
Mitch Dawson <86007219+mitchdawson1982@users.noreply.github.com>
2024-10-23 15:02:43 +0100

    Report issues with metadata (#953)
    * initial commit
    
    * initial commit
    
    * add notify vars
    
    * update actions and deployments
    
    * update env template
    
    * add context processor for notify enabled flag, update settings and template
    
    * add error messages in template for additional info
    
    * fix typo in test
    
    * PR Updates
    
    * update lock file
    
    * remove entity link below heading
    
    * Fmd 911 workflow changes (#971)
    
    * report an issue workflow changes and test fix
    
    * report an issue workflow changes and test fix
    
    * fix: remove dynamic content from translations (#972)
    
    The po file was a bit messed up, because I stuck an extra translation
    for "Justice Data" in there. When makemessages ran again, it was
    removing this translation. Also there were some missing msgstrs and
    fuzzy matches.
    
    This commit moves the justice-data -> Justice Data translation into the
    code instead.
    
    * initial commit
    
    * update actions and deployments
    
    * PR Updates
    
    * rebase errors
    
    * remove redundant code
    
    * update notify service mock
    
    * update str formatting
    
    ---------
    
    Co-authored-by: Mat <MatMoore@users.noreply.github.com>

--- lib/datahub-client/data_platform_catalogue/client/datahub_client.py
+++ lib/datahub-client/data_platform_catalogue/client/datahub_client.py
@@ -3,33 +3,6 @@ import logging
 from importlib.resources import files
 from typing import Sequence
 
-from datahub.configuration.common import ConfigurationError
-from datahub.emitter import mce_builder
-from datahub.emitter.mcp import MetadataChangeProposalWrapper
-from datahub.ingestion.graph.client import DatahubClientConfig, DataHubGraph
-from datahub.ingestion.source.common.subtypes import (
-    DatasetContainerSubTypes,
-    DatasetSubTypes,
-)
-from datahub.metadata import schema_classes
-from datahub.metadata.com.linkedin.pegasus2avro.common import DataPlatformInstance
-from datahub.metadata.schema_classes import (
-    ChangeTypeClass,
-    ContainerClass,
-    ContainerPropertiesClass,
-    DatasetPropertiesClass,
-    DomainPropertiesClass,
-    DomainsClass,
-    OtherSchemaClass,
-    OwnerClass,
-    OwnershipClass,
-    OwnershipTypeClass,
-    SchemaFieldClass,
-    SchemaFieldDataTypeClass,
-    SchemaMetadataClass,
-    SubTypesClass,
-)
-
 from data_platform_catalogue.client.exceptions import (
     AspectDoesNotExist,
     ConnectivityError,
@@ -70,6 +43,32 @@ from data_platform_catalogue.search_types import (
     SearchFacets,
     SearchResponse,
     SortOption,
+)
+from datahub.configuration.common import ConfigurationError
+from datahub.emitter import mce_builder
+from datahub.emitter.mcp import MetadataChangeProposalWrapper
+from datahub.ingestion.graph.client import DatahubClientConfig, DataHubGraph
+from datahub.ingestion.source.common.subtypes import (
+    DatasetContainerSubTypes,
+    DatasetSubTypes,
+)
+from datahub.metadata import schema_classes
+from datahub.metadata.com.linkedin.pegasus2avro.common import DataPlatformInstance
+from datahub.metadata.schema_classes import (
+    ChangeTypeClass,
+    ContainerClass,
+    ContainerPropertiesClass,
+    DatasetPropertiesClass,
+    DomainPropertiesClass,
+    DomainsClass,
+    OtherSchemaClass,
+    OwnerClass,
+    OwnershipClass,
+    OwnershipTypeClass,
+    SchemaFieldClass,
+    SchemaFieldDataTypeClass,
+    SchemaMetadataClass,
+    SubTypesClass,
 )
 
 logger = logging.getLogger(__name__)
--- tests/feedback/test_forms.py
+++ tests/feedback/test_forms.py
@@ -1,14 +1,14 @@
 import pytest
 
-from feedback.forms import FeedbackForm
-from feedback.models import Feedback
+from feedback.forms import FeedbackForm, IssueForm
+from feedback.models import Feedback, Issue
 
 
-def test_invalid_form():
+def test_invalid_feedback_form():
     assert not FeedbackForm({}).is_valid()
 
 
-def test_valid_form():
+def test_valid_feedback_form():
     assert FeedbackForm({"satisfaction_rating": 5}).is_valid()
     assert FeedbackForm(
         {"satisfaction_rating": 1, "how_can_we_improve": "blah"}
@@ -16,7 +16,7 @@ def test_valid_form():
 
 
 @pytest.mark.django_db
-def test_form_saves_to_db():
+def test_feedback_form_saves_to_db():
     form = FeedbackForm({"satisfaction_rating": 1, "how_can_we_improve": "blah"})
     form.save()
 
@@ -24,3 +24,50 @@ def test_form_saves_to_db():
     assert saved
     assert saved.satisfaction_rating == 1
     assert saved.how_can_we_improve == "blah"
+
+
+def test_valid_report_issue_form():
+    assert IssueForm(
+        {
+            "reason": "Other",
+            "additional_info": "a" * 10,
+        }
+    ).is_valid()
+
+
+def test_report_issue_form_invalid_additinal_info_length():
+    form = IssueForm(
+        {
+            "reason": "Other",
+            "additional_info": "a" * 9,
+        }
+    )
+    assert not form.is_valid()
+    assert (
+        "Ensure this value has at least 10 characters (it has 9)."
+        == form.errors["additional_info"][0]
+    )
+
+
+def test_report_issue_form_invalid_user_email():
+    form = IssueForm(
+        {"reason": "Other", "additional_info": "a" * 10, "user_email": "invalid_email"}
+    )
+    assert not form.is_valid()
+    assert "Enter a valid email address." == form.errors["user_email"][0]
+
+
+@pytest.mark.django_db
+def test_report_issue_form_saves_to_db():
+    form = IssueForm(
+        {
+            "reason": "Other",
+            "additional_info": "a" * 10,
+        }
+    )
+    form.save()
+
+    saved = Issue.objects.first()
+    assert saved
+    assert saved.reason == "Other"
+    assert saved.additional_info == "a" * 10
