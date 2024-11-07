4fccede3dee2e1ad14d19032b0742b6523b2a302
Mat Moore <MatMoore@users.noreply.github.com>
2024-11-07 15:53:32 +0000

    feat: add classes to generate table CSVs

---
+++ home/service/details_csv.py
@@ -0,0 +1,69 @@
+from home.service.details import (
+    DashboardDetailsService,
+    DatabaseDetailsService,
+    DatasetDetailsService,
+)
+
+
+class DatasetDetailsCsvFormatter:
+    def __init__(self, details_service: DatasetDetailsService):
+        self.details_service = details_service
+
+    def data(self):
+        return [
+            (
+                column.name,
+                column.display_name,
+                column.is_primary_key,
+                column.type,
+                column.nullable,
+                column.description,
+            )
+            for column in self.details_service.table_metadata.column_details
+        ]
+
+    def headers(self):
+        return [
+            "name",
+            "display_name",
+            "is_primary_key",
+            "type",
+            "nullable",
+            "description",
+        ]
+
+
+class DatabaseDetailsCsvFormatter:
+    def __init__(self, details_service: DatabaseDetailsService):
+        self.details_service = details_service
+
+    def data(self):
+        return [
+            (
+                table.entity_ref.urn,
+                table.entity_ref.display_name,
+                table.description,
+            )
+            for table in self.details_service.entities_in_database
+        ]
+
+    def headers(self):
+        return [
+            "urn",
+            "display_name",
+            "description",
+        ]
+
+
+class DashboardDetailsCsvFormatter:
+    def __init__(self, details_service: DashboardDetailsService):
+        self.details_service = details_service
+
+    def data(self):
+        return [
+            (chart.entity_ref.urn, chart.entity_ref.display_name, chart.description)
+            for chart in self.details_service.children
+        ]
+
+    def headers(self):
+        return ["urn", "display_name", "description"]
