53b939dbc5fd58af3191e004501069b308c802d7
Matt <38562764+LavMatt@users.noreply.github.com>
2024-12-13 14:56:45 +0000

    fix: properly set is nullable column waffle switch (#1171)
    * fix: properly set is nullable column waffle switch
    
    * add is nullable waffle switch to readme

--- scripts/app-entrypoint.sh
+++ scripts/app-entrypoint.sh
@@ -13,6 +13,6 @@ fi
 python manage.py migrate
 python manage.py waffle_switch search-sort-radio-buttons off --create # create switch with default setting
 python manage.py waffle_switch display-result-tags off --create # create display tags switch with default off
-
+python manage.py waffle_switch show_is_nullable_in_table_details_column off --create # create isnullable column switch with default off
 
 gunicorn --bind 0.0.0.0:8000 core.wsgi:application --workers 2 --threads 4
