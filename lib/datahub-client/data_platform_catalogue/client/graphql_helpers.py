a2cb14410a6bffafb5a435ebddb7ba8924ccd30b
Murdo Moyse <murdo.moyse@digital.justice.gov.uk>
2024-12-30 11:09:08 +0000

    Collect strings into variables

--- lib/datahub-client/data_platform_catalogue/client/graphql_helpers.py
+++ lib/datahub-client/data_platform_catalogue/client/graphql_helpers.py
@@ -1,10 +1,13 @@
 from importlib.resources import files
 
+GRAPHQL_FILES_PATH = "data_platform_catalogue.client.graphql"
+GRAPHQL_FILE_EXTENSION = ".graphql"
+
 
 def get_graphql_query(graphql_query_file_name: str) -> str:
     query_text = (
-        files("data_platform_catalogue.client.graphql")
-        .joinpath(f"{graphql_query_file_name}.graphql")
+        files(GRAPHQL_FILES_PATH)
+        .joinpath(f"{graphql_query_file_name}{GRAPHQL_FILE_EXTENSION}")
         .read_text()
     )
     return query_text
