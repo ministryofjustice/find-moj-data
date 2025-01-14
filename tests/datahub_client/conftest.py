47204dd4c60351aa49e4d01f6151e20c9d5f547b
Mitch Dawson <Mitchell.Dawson@justice.gov.uk>
2025-01-14 14:23:57 +0000

    inline entity timestamp fixture

--- tests/datahub_client/conftest.py
+++ tests/datahub_client/conftest.py
@@ -1,25 +1,8 @@
-from datetime import datetime, timedelta
 from pathlib import Path
 from typing import Any, Dict
 
 import pytest
 from datahub.metadata.schema_classes import DomainPropertiesClass
-
-from datahub_client.entities import (
-    AccessInformation,
-    CustomEntityProperties,
-    DataSummary,
-    DomainRef,
-    EntityRef,
-    EntitySummary,
-    FurtherInformation,
-    GlossaryTermRef,
-    Governance,
-    OwnerRef,
-    RelationshipType,
-    TagRef,
-    UsageRestrictions,
-)
 
 from .test_helpers.graph_helpers import MockDataHubGraph
 from .test_helpers.mce_helpers import check_golden_file
@@ -67,85 +50,3 @@ def pytest_addoption(parser):
         default=False,
     )
     parser.addoption("--copy-output-files", action="store_true", default=False)
-
-
-@pytest.fixture
-def entity_data_with_timestamps_in_future():
-    future_timestamp = datetime.now() + timedelta(days=1)
-    return {
-        "urn": "urn:li:chart:(justice-data,absconds)",
-        "display_name": "Absconds",
-        "name": "Absconds",
-        "fully_qualified_name": "",
-        "description": "Number of absconds",
-        "relationships": {
-            RelationshipType.PARENT: [
-                EntitySummary(
-                    entity_ref=EntityRef(
-                        urn="urn:li:database:example", display_name="example"
-                    ),
-                    description="entity for an example",
-                    entity_type="DATABASE",
-                    tags=[
-                        TagRef(
-                            urn="urn:li:tag:dc_display_in_catalogue",
-                            display_name="dc_display_in_catalogue",
-                        )
-                    ],
-                )
-            ]
-        },
-        "domain": DomainRef(display_name="HMPPS", urn="urn:li:domain:HMCTS"),
-        "governance": Governance(
-            data_owner=OwnerRef(
-                display_name="John Doe",
-                email="jogn.doe@justice.gov.uk",
-                urn="urn:li:corpuser:john.doe",
-            ),
-            data_stewards=[
-                OwnerRef(
-                    display_name="Jane Smith",
-                    email="jane.smith@justice.gov.uk",
-                    urn="urn:li:corpuser:jane.smith",
-                )
-            ],
-            data_custodians=[
-                OwnerRef(
-                    display_name="Rosanne Columns",
-                    email="rosanne.columns@justice.gov.uk",
-                    urn="urn:li:corpuser:rosanne.columns",
-                )
-            ],
-        ),
-        "glossary_terms": [
-            GlossaryTermRef(
-                display_name="Essential Shared Data Asset (ESDA)",
-                urn="urn:li:glossaryTerm:ESDA",
-                description="An ESDA is...",
-            )
-        ],
-        "metadata_last_ingested": future_timestamp,
-        "created": future_timestamp,
-        "data_last_modified": future_timestamp,
-        "platform": EntityRef(urn="urn:li:dataPlatform:kafka", display_name="Kafka"),
-        "custom_properties": CustomEntityProperties(
-            usage_restrictions=UsageRestrictions(
-                dpia_required=False, dpia_location="OneTrust"
-            ),
-            access_information=AccessInformation(
-                dc_where_to_access_dataset="Analytical platform",
-                source_dataset_name="stg_xhibit_bw_history",
-                s3_location="s3://alpha-hmpps-reports-data",
-                dc_access_requirements="Access granted on request",
-            ),
-            data_summary=DataSummary(row_count=123, refresh_period="Daily"),
-            further_information=FurtherInformation(
-                dc_slack_channel_name="#data-engineering",
-                dc_slack_channel_url="https://hmpps-data-engineering.slack.com",
-                dc_teams_channel_name="Data team",
-                dc_teams_channel_url="https://teams.microsoft.com/l/channel/123",
-                dc_team_email="best-data-team@justice.gov.uk",
-            ),
-        ),
-        "tags_to_display": ["nomis", "data-warehouse"],
-    }
