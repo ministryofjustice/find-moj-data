816b12a8a4c8da9bd122da0a1cb231037140e95f
Mat Moore <MatMoore@users.noreply.github.com>
2025-01-28 12:29:59 +0000

    build: create feature flags for the new homepage design

--- scripts/app-entrypoint.sh
+++ scripts/app-entrypoint.sh
@@ -14,5 +14,7 @@ python manage.py migrate
 python manage.py waffle_switch search-sort-radio-buttons off --create # create switch with default setting
 python manage.py waffle_switch display-result-tags off --create # create display tags switch with default off
 python manage.py waffle_switch show_is_nullable_in_table_details_column off --create # create isnullable column switch with default off
+python manage.py waffle_flag home_variant_a --testing --create
+python manage.py waffle_flag home_variant_b --testing --create
 
 gunicorn --bind 0.0.0.0:8000 core.wsgi:application --workers 2 --threads 4
