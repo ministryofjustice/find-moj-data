1f76da40e517c1e78492998629b7c3fb92f31b0b
Mitch Dawson <Mitchell.Dawson@justice.gov.uk>
2025-02-24 16:31:34 +0000

    chore: Remove testing waffle flags for home variants from entrypoint script

--- scripts/app-entrypoint.sh
+++ scripts/app-entrypoint.sh
@@ -15,7 +15,5 @@ python manage.py waffle_switch search-sort-radio-buttons off --create # create s
 python manage.py waffle_switch display-result-tags off --create # create display tags switch with default off
 python manage.py waffle_switch show_is_nullable_in_table_details_column off --create # create isnullable column switch with default off
 python manage.py waffle_switch new_subject_areas on --create # remove this once deployed
-python manage.py waffle_flag home_variant_a --testing --create
-python manage.py waffle_flag home_variant_b --testing --create
 
 gunicorn --bind 0.0.0.0:8000 core.wsgi:application --workers 2 --threads 4
