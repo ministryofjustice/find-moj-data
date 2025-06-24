7bcba5a467cd4fdfee0be5d864c43b57d8e47dd4
Matt <38562764+LavMatt@users.noreply.github.com>
2025-06-24 16:53:35 +0100

    feat: Add userguide to front end (#1354)
    * feat: add PoC user guide app with sidebar and markdown content rendering
    
    * fix: copilot pr suggestion
    
    * feat: improve userguide style and navigation
    
    * feat: add userguide directory to Dockerfile
    
    * Feat: Enable serving media files in debug mode
    
    * Add media files
    
    * Remove outdated user guide sections: metadata, glossary, and overview
    
    * Fix: Correct whitespace in for loop in glossary template
    
    * Feat: Add About and Other Sources documentation sections
    
    * Fix: Update sidebar item spacing and improve markdown file loading
    
    * Fix: Refactor user guide layout for improved structure and readability
    
    * Remove media files
    
    * Fix: Update image paths in About.md to use static assets
    
    * Fix: Update userguide_view slug parameter to lowercase for consistency
    
    * Add About and Create a Derived Table documentation; introduce Other Sources section
    
    * Fix: Add return type hints for make_side_bar_items and get_markdown_content functions
    
    * Fix: Update User guide link text for consistency
    
    * Fix: Remove static media URL configuration from debug toolbar urlpatterns
    
    * Fix: Adjust image dimensions in About.md for better display
    
    * Move user guide template location
    
    * Fix: Ensure guide images are copied during the Docker build process
    
    * Fix: Simplify section headings in About.md for clarity
    
    * Fix: Update section headings for clarity in Create a derived table.md
    
    * Fix: Exclude static assets directory from .gitignore to allow guide images
    
    * Fix: Unignore guide images directory in .gitignore to allow proper asset inclusion
    
    * Add guide images for example table entity and FMD process schematic
    
    * Fix: Update .gitignore to include specific static asset directories and files for proper asset management
    
    * Fix: Update contact us link to use URL template and add aria-current attribute for active state
    
    * Fix: Update .gitignore to include guide images while excluding other static assets
    
    * Replace jpg file with svg
    
    * Fix: Update image formats and dimensions in About.md for better display and accessibility
    
    * Fix: Move pre and code styles from base.html to base.scss for better organization and maintainability
    
    * Fix: Add glossary and user guide links to primary navigation and update contact us link for correct path
    
    * Fix: Remove unused markdown extensions from userguide_view for cleaner processing
    
    * Add Contact Us section with feedback and support links
    
    * Fix: Enhance sidebar navigation in user guide with subheader links for improved usability
    
    * Fix: Update .gitignore to include SVG images in the guide assets for proper tracking
    
    * Add enhanced user guide functionality to manage active navigation states based on URL hash
    
    * Refactor user guide layout for improved sidebar navigation and content structure
    
    * Add GOV.UK styles for anchor tags in markdown processing
    
    * Fix formatting issues in 'Other sources.md' for improved readability
    
    * Replace header titles with links in side navigation for improved accessibility
    
    * Remove unignore comment for guide images directory in .gitignore
    
    ---------
    
    Co-authored-by: Mitch Dawson <86007219+mitchdawson1982@users.noreply.github.com>
    Co-authored-by: Mitch Dawson <Mitchell.Dawson@justice.gov.uk>

---
+++ userguide/views.py
@@ -0,0 +1,129 @@
+import os
+import re
+from dataclasses import dataclass, field
+from typing import Iterator, List, Tuple
+
+import markdown
+from django.http import Http404
+from django.shortcuts import render
+from django.utils.text import slugify
+from markdown.extensions import Extension
+from markdown.treeprocessors import Treeprocessor
+
+DOCS_DIR = os.path.join(os.path.dirname(__file__), "content")
+
+
+@dataclass
+class SidebarItem:
+    header: str
+    header_slug: str
+    subheader: list
+
+
+@dataclass
+class Sidebar:
+    sidebar: List[SidebarItem] = field(default_factory=list)
+    slug_to_header: dict = field(default_factory=dict)
+
+    def add_item(self, item: SidebarItem):
+        """Appends a SidebarItem to the sidebar list."""
+        self.sidebar.append(item)
+        self.slug_to_header[item.header_slug] = item.header
+
+    def __iter__(self) -> Iterator[Tuple[str, str, List[str]]]:
+        """Makes the Sidebar iterable."""
+        for item in self.sidebar:
+            yield item.header, item.header_slug, item.subheader,
+
+
+class CustomClassProcessor(Treeprocessor):
+    """
+    Adds gov.uk classes to elements in the markdown file contents.
+    """
+
+    def generate_id(self, text):
+        return re.sub(r"[^a-zA-Z0-9]+", "-", text).strip("-").lower()
+
+    def run(self, root):
+        for element in root.iter():
+            tag_class_map = {
+                "h1": "govuk-heading-xl",
+                "h2": "govuk-heading-l",
+                "h3": "govuk-heading-m",
+                "ul": "govuk-list govuk-list--bullet govuk-list--spaced",
+                "a": "govuk-link govuk-link--no-visited-state govuk-link--no-underline",
+                "table": "govuk-table",
+                "thead": "govuk-table__header",
+                "tr": "govuk-table__row",
+                "td": "govuk-table__cell govuk-body-s",
+                "tbody": "govuk-table__body",
+            }
+
+            if element.tag in tag_class_map:
+                element.set("class", tag_class_map[element.tag])
+                if element.tag in ["h1", "h2", "h3"]:
+                    element.set("id", self.generate_id(element.text))
+
+
+class CustomClassExtension(Extension):
+    def extendMarkdown(self, md):
+        md.treeprocessors.register(CustomClassProcessor(md), "govuk_class", 5)
+
+
+def make_side_bar_items() -> Sidebar:
+    """
+    Loads all markdown files in the content directory
+    and creates the sidebar object
+    """
+    sidebar = Sidebar()
+    docs = sorted(doc for doc in os.listdir(DOCS_DIR) if doc.endswith(".md"))
+    for file in docs:
+        with open(os.path.join(DOCS_DIR, file), "r", encoding="utf-8") as f:
+            content = f.read()
+            subheader = []
+            for line in content.split("\n"):
+                if line.startswith("# "):  # Header level 1
+                    header = line[2:].strip()
+                elif line.startswith("##"):  # Header level 2 or higher
+                    subheader.append(line[3:].strip())
+
+            sidebar.add_item(
+                SidebarItem(
+                    header=header,
+                    header_slug=slugify(header),
+                    subheader=subheader,
+                )
+            )
+    return sidebar
+
+
+def get_markdown_content(filename) -> str:
+    """The filename should be the header slug without the .md extension, and in the same case.
+    i.e, "# About" for "About.md".
+    """
+    file_path = os.path.normpath(os.path.join(DOCS_DIR, filename + ".md"))
+    if not file_path.startswith(DOCS_DIR):
+        raise Http404("Page not found")
+    with open(file_path, "r", encoding="utf-8") as f:
+        return f.read()
+
+
+def userguide_view(request, slug="about"):
+    # Generate a sidebar menu from the file structure
+    sidebar_items = make_side_bar_items()
+    original_header = sidebar_items.slug_to_header.get(slug, slug)
+    content = get_markdown_content(original_header)
+    html_output = markdown.markdown(
+        content,
+        extensions=[
+            "fenced_code",
+            "attr_list",
+            CustomClassExtension(),
+            "tables",
+        ],
+    )
+    return render(
+        request,
+        "userguide_page.html",
+        {"content": html_output, "sidebar": sidebar_items},
+    )
