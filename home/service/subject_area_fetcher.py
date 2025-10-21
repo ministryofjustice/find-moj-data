77e844111f8d1403d00c46eac4e972e554cc466e
Mitch Dawson <Mitchell.Dawson@justice.gov.uk>
2025-10-21 14:50:01 +0100

    Enhance SubjectAreaFetcher to support sorting by total entities and filter zero entities

--- home/service/subject_area_fetcher.py
+++ home/service/subject_area_fetcher.py
@@ -10,11 +10,43 @@ class SubjectAreaFetcher(GenericService):
     Fetches subject areas with the total number of associated entities from the backend.
     """
 
-    def __init__(self, filter_zero_entities: bool = True):
+    def __init__(
+        self, filter_zero_entities: bool = True, sort_total_descending: bool = False
+    ):
         self.client = self._get_catalogue_client()
         self.cache_key = "list_subject_areas"
         self.cache_timeout_seconds = 300
         self.filter_zero_entities = filter_zero_entities
+        self.sort_total_descending = sort_total_descending
+
+    def filter_and_sort(
+        self, subject_areas: list[SubjectAreaOption]
+    ) -> list[SubjectAreaOption]:
+        result = subject_areas
+        if self.filter_zero_entities:
+            result = [subject_area for subject_area in result if subject_area.total > 0]
+
+        # We want 'Miscellaneous' to always be last, so we obtain the index and pop it out.
+        miscellanoeus_sa_index = next(
+            (i for i, d in enumerate(result) if d.urn == "urn:li:tag:Miscellaneous"),
+            None,
+        )
+        miscellanouse_sa = None
+        if miscellanoeus_sa_index is not None:
+            miscellanouse_sa = result.pop(miscellanoeus_sa_index)
+
+        if self.sort_total_descending:
+            # Sort by total descending
+            result = sorted(result, key=lambda d: d.total, reverse=True)
+        else:
+            # Sort by name ascending
+            result = sorted(result, key=lambda d: d.urn)
+
+        # Append 'Miscellaneous' to the end of the list
+        if miscellanouse_sa is not None:
+            result.append(miscellanouse_sa)
+
+        return result
 
     def fetch(self) -> list[SubjectAreaOption]:
         """
@@ -25,11 +57,6 @@ class SubjectAreaFetcher(GenericService):
         result = cache.get(self.cache_key, version=2)
         if not result:
             result = self.client.list_subject_areas()
-            result = sorted(result, key=lambda d: d.urn)
-            cache.set(
-                self.cache_key, result, timeout=self.cache_timeout_seconds, version=2
-            )
-
-        if self.filter_zero_entities:
-            result = [subject_area for subject_area in result if subject_area.total > 0]
+            cache.set(self.cache_key, result, self.cache_timeout_seconds, version=2)
+            result = self.filter_and_sort(result)
         return result
