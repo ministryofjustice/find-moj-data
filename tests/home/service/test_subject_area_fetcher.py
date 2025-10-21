30d715bc458c548f6a3c2ae22c6467e2afeed9c0
Mitch Dawson <Mitchell.Dawson@justice.gov.uk>
2025-10-21 14:50:38 +0100

    Refactor tests for SubjectAreaFetcher to improve clarity and organization

--- tests/home/service/test_subject_area_fetcher.py
+++ tests/home/service/test_subject_area_fetcher.py
@@ -2,14 +2,28 @@ from home.service.subject_area_fetcher import SubjectAreaFetcher
 
 
 class TestSubjectAreaFetcher:
-    def test_list_all_subject_areas(self, mock_catalogue) -> None:
-        subject_area_options = SubjectAreaFetcher(False).fetch()
-        assert len(subject_area_options) == 4
-
-    def test_list_non_empty_subject_areas(self, mock_catalogue):
-        subject_area_options = SubjectAreaFetcher(True).fetch()
+    def test_remove_zero_entity_subject_areas(self, mock_catalogue) -> None:
+        subject_area_options = SubjectAreaFetcher(
+            filter_zero_entities=True, sort_total_descending=False
+        ).fetch()
         assert len(subject_area_options) == 3
         assert all(
             subject_area_option.total > 0
             for subject_area_option in subject_area_options
         )
+
+    def test_sort_subject_areas_by_total_descending(self, mock_catalogue) -> None:
+        subject_area_options = SubjectAreaFetcher(
+            filter_zero_entities=False, sort_total_descending=True
+        ).fetch()
+        totals = [
+            subject_area_option.total for subject_area_option in subject_area_options
+        ]
+        assert totals == sorted(totals, reverse=True)
+
+    def test_sort_subject_areas_by_name_ascending(self, mock_catalogue) -> None:
+        subject_area_options = SubjectAreaFetcher(
+            filter_zero_entities=False, sort_total_descending=False
+        ).fetch()
+        urns = [subject_area_option.urn for subject_area_option in subject_area_options]
+        assert urns == sorted(urns)
