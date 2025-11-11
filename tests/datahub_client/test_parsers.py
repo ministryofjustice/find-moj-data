d062bd3e2179e23a9a8b586964e1dc18e9085fb7
Mark McKinlay <mark@mckinlay.io>
2025-11-11 16:44:20 +0000

    Updated tests

--- tests/datahub_client/test_parsers.py
+++ tests/datahub_client/test_parsers.py
@@ -918,15 +918,15 @@ class TestEntityParser:
     @pytest.mark.parametrize(
         "tags, expected_refresh_period",
         [
-            ([TagRef(display_name="daily_opg", urn="urn:li:tag:daily_opg")], "Every Day"),
-            ([TagRef(display_name="monthly", urn="urn:li:tag:monthly")], "Every Month"),
-            ([TagRef(display_name="dc_cadet", urn="urn:li:tag:dc_cadet")], ""),
+            ([TagRef(display_name="daily_opg", urn="urn:li:tag:daily_opg")], "Every day"),
+            ([TagRef(display_name="monthly", urn="urn:li:tag:monthly")], "Every month"),
+            ([TagRef(display_name="dc_cadet", urn="urn:li:tag:dc_cadet")], None),
             (
                 [
                     TagRef(display_name="Every Day", urn="urn:li:tag:dc_cadet"),
                     TagRef(display_name="monthly", urn="urn:li:tag:dc_cadet"),
                 ],
-                "Every Month",
+                "Every month",
             ),
         ],
     )
