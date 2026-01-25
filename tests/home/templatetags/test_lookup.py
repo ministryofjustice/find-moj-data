321a0f80477a04d304cdd6bfb51aa095ca42d954
Mitch Dawson <Mitchell.Dawson@justice.gov.uk>
2026-01-25 19:33:30 +0000

    remove redundant get_count tests

--- tests/home/templatetags/test_lookup.py
+++ tests/home/templatetags/test_lookup.py
@@ -1,6 +1,6 @@
 import pytest
 
-from home.templatetags.lookup import get_count, lookup
+from home.templatetags.lookup import lookup
 
 
 @pytest.mark.parametrize(
@@ -14,27 +14,3 @@ from home.templatetags.lookup import get_count, lookup
 )
 def test_lookup(input_list, lookup_dict, output_list):
     assert lookup(input_list, lookup_dict) == output_list
-
-
-class TestGetCount:
-    """Tests for the get_count template filter."""
-
-    def test_get_count_returns_value_when_key_exists(self):
-        lookup_dict = {"TABLE": 100, "CHART": 50}
-        assert get_count(lookup_dict, "TABLE") == 100
-        assert get_count(lookup_dict, "CHART") == 50
-
-    def test_get_count_returns_zero_when_key_missing(self):
-        lookup_dict = {"TABLE": 100}
-        assert get_count(lookup_dict, "MISSING") == 0
-
-    def test_get_count_returns_zero_when_dict_is_none(self):
-        assert get_count(None, "TABLE") == 0
-
-    def test_get_count_returns_zero_when_dict_is_empty(self):
-        assert get_count({}, "TABLE") == 0
-
-    def test_get_count_with_integer_keys(self):
-        lookup_dict = {1: 10, 2: 20}
-        assert get_count(lookup_dict, 1) == 10
-        assert get_count(lookup_dict, 3) == 0
