527d457b333f527b93175a84520a641b05825d59
Mitch Dawson <Mitchell.Dawson@justice.gov.uk>
2026-02-06 10:37:51 +0000

    fix: simplify test logic for STAFF email validation

--- tests/core/test_staff_list.py
+++ tests/core/test_staff_list.py
@@ -1,9 +1,10 @@
+import pytest
 from django.conf import settings
 from email_validator import validate_email
 
 
+@pytest.mark.skipif(not settings.STAFF, reason="STAFF setting is not defined or not available in the environment")
 def test_middleware_renders_bad_request_response():
     staff_list = settings.STAFF
-    if staff_list:
-        for staff_member in staff_list:
-            assert validate_email(staff_member, check_deliverability=False)
+    for staff_member in staff_list:
+        assert validate_email(staff_member, check_deliverability=False)
