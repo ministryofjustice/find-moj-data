4f3886127290bc6459ec9f954e013f9c93b938d3
Mark McKinlay <mark@mckinlay.io>
2026-02-06 10:16:52 +0000

    Fixed Test_staff_list to handle empty STAFF list

--- tests/core/test_staff_list.py
+++ tests/core/test_staff_list.py
@@ -4,5 +4,6 @@ from email_validator import validate_email
 
 def test_middleware_renders_bad_request_response():
     staff_list = settings.STAFF
-    for staff_member in staff_list:
-        assert validate_email(staff_member, check_deliverability=False)
+    if staff_list:
+        for staff_member in staff_list:
+            assert validate_email(staff_member, check_deliverability=False)
