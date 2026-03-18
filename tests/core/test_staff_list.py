95f5a2f82bf52f061301db59cdd2834de7db030c
Mark McKinlay <mark@mckinlay.io>
2026-03-18 09:02:41 +0000

    Test fix

--- tests/core/test_staff_list.py
+++ tests/core/test_staff_list.py
@@ -6,5 +6,6 @@ from email_validator import validate_email
 @pytest.mark.skipif(not settings.STAFF, reason="STAFF setting is not defined or not available in the environment")
 def test_middleware_renders_bad_request_response():
     staff_list = settings.STAFF
-    for staff_member in staff_list:
-        assert validate_email(staff_member, check_deliverability=False)
+    if staff_list:
+        for staff_member in staff_list:
+            assert validate_email(staff_member, check_deliverability=False)
